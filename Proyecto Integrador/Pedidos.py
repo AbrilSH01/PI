de337d013ac21d2c3d22ae884edfd4d7189c6506
PabloRC0525 <121038827@upq.edu.mx>
2023-03-06 15:25:58 -0600

    Se crearon botones para los pedidos,

--- Proyecto Integrador/Pedidos.py
+++ Proyecto Integrador/Pedidos.py
@@ -8,44 +8,105 @@ ventana.geometry("600x400")
 seccion1=Frame(ventana,bg="green")
 seccion1.pack(expand=True,fill='both')
         
-menu = Label(ventana, text="Menu", bg="green")
-menu.place(x=50,y=0)
-Bebidas = Label(ventana, text="Bebidas:", bg="green")
-Bebidas.place(x=50,y=20)
-Bebidas1 = Label(ventana, text="1.Late $25 2.Espresso $30 3.Matcha $30 4.Frappe $50 ", bg="green")
-Bebidas1.place(x=50,y=40)
-Comida = Label(ventana, text="Comida:", bg="green")
-Comida.place(x=50,y=60)
-Comida1 = Label(ventana, text="5.Pizza $120 6.Sandwich $50 7.Hamburguesa $50  8.Hot Dog $30", bg="green")
-Comida1.place(x=50,y=80)
-Postres = Label(ventana, text="Postres:", bg="green")
-Postres.place(x=50,y=100)
-Postres1 = Label(ventana, text="9.cheesecake $25 10.Cupcake $30 11.Croisant $30 12.Pay $25", bg="green")
-Postres1.place(x=50,y=120)
-seleccionar = ["Late","Espresso","Matcha","Frappe", "Pizza","Sandwich","Hamburguesa","Hot Dog","cheesecake","Cupcake","Croisant","Pay"]
-opcion_seleccionada = tk.StringVar()
-Seleccionar = ttk.Combobox(textvariable=opcion_seleccionada, values=seleccionar)
-Seleccionar.place(x=200,y=150)
+
 Total=0
-def Añadir():
+def late():
     global Total
-    if Seleccionar.get() == "Late" or Seleccionar.get() == "cheesecake" or Seleccionar.get() == "Pay":
-        Total = Total + 25
-    elif Seleccionar.get() == "Espresso" or Seleccionar.get() == "Matcha" or Seleccionar.get() == "Hot Dog" or Seleccionar.get() == "Cupcake" or Seleccionar.get() == "Croisant":
-        Total = Total + 30
-    elif Seleccionar.get() == "Frappe" or Seleccionar.get() == "Hamburguesa" or Seleccionar.get() == "Sandwich":
-        Total = Total + 50
-    elif Seleccionar.get() == "Pizza":
-        Total = Total + 120
-    else:
-        messagebox.showerror("Error", "Seleccione un producto")
+    messagebox.showinfo('Pedido','Se ha agregado un Late +$25')
+    Total = Total + 25
+def pay():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado un Pay + $25')
+    Total = Total + 25
+def ck():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Cheesecake +$25')
+    Total = Total + 25
+def mt():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Matcha +$30')
+    Total = Total + 30
+def xp():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Espresso +$30')
+    Total = Total + 30
+def HD():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Hot dog +$30')
+    Total = Total + 30
+def F():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Frappe +$50')
+    Total = Total + 50  
+def H():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Hamburguesa +$50')
+    Total = Total + 50 
+def chanwis():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Sandwich +$50')
+    Total = Total + 50 
+def c120():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Pizza +$120')
+    Total = Total + 120
+
 
 def Tot():
-    messagebox.showinfo("Total", "El total de su compra es: " + str(Total))
+    if Total==0:
+        messagebox.showerror("Error","Seleccione minimo una opcion")
+    else:
+        messagebox.showinfo("Total", "El total de su compra es: " + str(Total))
 
 
-Agregar = Button(seccion1, text="Agregar", bg="#255",command=Añadir)
-Agregar.place(x=200,y=200)
+
 total=Button(seccion1, text="Cobrar", bg="#255",command=Tot)
 total.place(x=150,y=200)
+
+Met = Label(ventana, text="Metodo de pago:", bg="green")
+Met.place(x=50,y=250)
+metodos = ["Efectivo", "Tarjeta"]
+opcion_seleccionada = tk.StringVar()
+Metodo = ttk.Combobox(textvariable=opcion_seleccionada, values=metodos)
+Metodo.place(x=200,y=250)
+def tarjeta():  
+    if Metodo.get()=="Tarjeta":
+        Num = Label(ventana, text="Numero de la tarjeta:", bg="green")
+        Num.place(x=50,y=280)
+        Numero = ttk.Entry(width= 30)
+        Numero.place(x=200,y=280)
+        fecha = Label(ventana, text="Fecha de vencimiento:", bg="green")
+        fecha.place(x=50,y=310)
+        Fecha = ttk.Entry(width= 30)
+        Fecha.place(x=200,y=310)
+        cvv = Label(ventana, text="CVV:", bg="green")
+        cvv.place(x=50,y=340)
+        CVV = ttk.Entry(width= 30,show="*")
+        CVV.place(x=200,y=340)
+        
+Enviar=Button(seccion1,text="Registrar",bg="#255748",command=tarjeta)
+Enviar.place(x=255,y=200)
+Mnu = Label(ventana, text="MENU:", bg="green")
+Mnu.place(x=20,y=10)
+Late=Button(seccion1,text="Late",bg="#255748",command=late)
+Late.place(x=50,y=50)
+Espresso=Button(seccion1,text="Espresso",bg="#255748",command=xp)
+Espresso.place(x=100,y=50)
+Frappe=Button(seccion1,text="Frappe",bg="#255748",command=F)
+Frappe.place(x=170,y=50)
+Matcha=Button(seccion1,text="Matcha",bg="#255748",command=mt)
+Matcha.place(x=230,y=50)
+Pizza=Button(seccion1,text="Pizza",bg="#255748",command=c120)
+Pizza.place(x=50,y=100)
+Hamburguesa=Button(seccion1,text="Hamburguesa",bg="#255748",command=H)
+Hamburguesa.place(x=100,y=100)
+HotDog=Button(seccion1,text="HotDog",bg="#255748",command=HD)
+HotDog.place(x=190,y=100)
+Sandwich=Button(seccion1,text="Sandwich",bg="#255748",command=chanwis)
+Sandwich.place(x=250,y=100)
+Cheesecake=Button(seccion1,text="Cheesecake",bg="#255748",command=pay)
+Cheesecake.place(x=50,y=150)
+Pay=Button(seccion1,text="Pay",bg="#255748",command=ck)
+Pay.place(x=130,y=150)
+
 ventana.mainloop()
