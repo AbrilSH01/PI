9a37b3402420a3317cc246d33a3ff79432d6bd0d
PabloRC0525 <121038827@upq.edu.mx>
2023-03-27 16:54:43 -0600

    Se colocó todo en una sola ventana y se conectó con la base de datos

--- Proyecto Integrador/Registro.py
+++ Proyecto Integrador/Registro.py
@@ -1,57 +1,174 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox
+import tkinter as tk
+from Registro_clase import *
 
+controlador = Registro_clase()
 
+def ejecutaInsert():
+    controlador.registro(Nombre.get(),Apellidos.get(),Correo.get(),Telefono.get(),Contraseña.get(),vacio)
+    
+def ejecutaInsert2():
+    controlador.Metod(Numero.get(),Fecha.get(),CVV.get(),vacio)
+    
+def ejecutaInsert3():
+    controlador.Pago(MET,Total)
+    
 ventana = Tk()
-ventana.title("Registro")
-ventana.geometry("600x400")
+ventana.title("Reinvent Cafe")
+ventana.geometry("500x300")
 
-def registro():
-        if Nombre.get()==vacio or Apellidos.get()==vacio or Correo.get()==vacio or Telefono.get()==vacio or Contraseña.get()==vacio:
-            messagebox.showerror("Error","Se deben llenar todos los datos")
-        else:
-            messagebox.showinfo("Correcto","Sus datos se han registrado correctamente")
+panel = ttk.Notebook(ventana)
+panel.pack(fill='both',expand='yes')
 
-seccion1=Frame(ventana,bg="green")
-seccion1.pack(expand=True,fill='both')
+pestaña1 = ttk.Frame(panel)
+pestaña2 = ttk.Frame(panel)
+pestaña3 = ttk.Frame(panel)
 
-Nom = Label(ventana, text="Ingrese su Nombre:", bg="green")
+#Registro de usuario
+titulo = Label(pestaña1,text="Registro de usuarios",fg="blue",font=("Modern",18)).pack()
+
+Nom = Label(pestaña1, text="Ingrese su Nombre:")
 Nom.place(x=50,y=50)
-
-Nombre = ttk.Entry(width= 30)
+Nombre = ttk.Entry(pestaña1,width= 30)
 Nombre.place(x=200,y=50)
     
 
-AP = Label(ventana, text="Ingrese sus Apellidos:", bg="green")
+AP = Label(pestaña1, text="Ingrese sus Apellidos:")
 AP.place(x=50,y=80)
-
-Apellidos = ttk.Entry(width= 30)
+Apellidos = ttk.Entry(pestaña1,width= 30)
 Apellidos.place(x=200,y=80)
 
 
-Corr = Label(ventana, text="Ingrese su Correo: ", bg="green")
+Corr = Label(pestaña1, text="Ingrese su Correo: ")
 Corr.place(x=50,y=110)
-
-Correo = ttk.Entry(width=30)
+Correo = ttk.Entry(pestaña1,width=30)
 Correo.place(x=200,y=110)
     
 
-Tel = Label(ventana, text="Ingrese su Telefono: ", bg="green")
+Tel = Label(pestaña1, text="Ingrese su Telefono: ")
 Tel.place(x=50,y=140)
-
-Telefono = ttk.Entry(width=30)
+Telefono = ttk.Entry(pestaña1,width=30)
 Telefono.place(x=200,y=140)
 
 
-Passw = Label(ventana, text="Ingrese su Contraseña: ", bg="green")
+Passw = Label(pestaña1, text="Ingrese su Contraseña: ")
 Passw.place(x=50,y=170)
-
-Contraseña = ttk.Entry(width=30,show="*")
+Contraseña = ttk.Entry(pestaña1,width=30,show="*")
 Contraseña.place(x=200,y=170)
 vacio=""
 
-BotonRegistrar = Button(seccion1,text="Registrar",bg="#255748",fg="white",command=registro)
-BotonRegistrar.pack()
+BotonRegistrar = Button(pestaña1,text="Registrar",bg="#255748",fg="white",command=ejecutaInsert)
+BotonRegistrar.place(y=200, x= 220)
 
+#Metodo de pago
+metodos= Label(pestaña2,text="Metodos de pago",fg="blue",font=("Modern",18)).pack()
+Met = Label(pestaña2, text="Metodo de pago:")
+Met.place(x=50,y=50)
+metodos = ["Efectivo", "Tarjeta"]
+opcion_seleccionada = tk.StringVar()
+Metodo = ttk.Combobox(pestaña2,textvariable=opcion_seleccionada, values=metodos)
+Metodo.place(x=200,y=50)
+Numero = ttk.Entry(pestaña2,width= 30)
+Fecha = ttk.Entry(pestaña2,width= 30)
+CVV = ttk.Entry(pestaña2,width= 30,show="*")
+Continuar=Button(pestaña2,text="Continuar",bg="#255748",command=ejecutaInsert2)
+def tarjeta():  
+    if Metodo.get()=="Tarjeta":
+        Num = Label(pestaña2, text="Numero de la tarjeta:")
+        Num.place(x=50,y=80)
+        Numero.place(x=200,y=80)
+        fecha = Label(pestaña2, text="Fecha de vencimiento:")
+        fecha.place(x=50,y=110)
+        Fecha.place(x=200,y=110)
+        cvv = Label(pestaña2, text="CVV:")
+        cvv.place(x=50,y=140)
+        CVV.place(x=200,y=140)
+        Continuar.place(x=300,y=200)
+    else:
+        Cont2=Button(pestaña2,text="Continuar",bg="#255748",command=ejecutaInsert2)
+        Cont2
+Enviar=Button(pestaña2,text="Registrar",bg="#255748",command=tarjeta)
+Enviar.place(x=210,y=200)
+
+#Menú
+
+Total=0
+def late():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado un Late +$25')
+    Total = Total + 25
+def pay():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado un Pay + $25')
+    Total = Total + 25
+def ck():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Cheesecake +$25')
+    Total = Total + 25
+def mt():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Matcha +$30')
+    Total = Total + 30
+def xp():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Espresso +$30')
+    Total = Total + 30
+def HD():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Hot dog +$30')
+    Total = Total + 30
+def F():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Frappe +$50')
+    Total = Total + 50  
+def H():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Hamburguesa +$50')
+    Total = Total + 50 
+def chanwis():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Sandwich +$50')
+    Total = Total + 50 
+def c120():
+    global Total
+    messagebox.showinfo('Pedido','Se ha agregado Pizza +$120')
+    Total = Total + 120
+
+
+
+        
+
+Mnu = Label(pestaña3,text="Menu",fg="blue",font=("Modern",18)).pack()
+Late=Button(pestaña3,text="Late",bg="#255748",command=late)
+Late.place(x=50,y=50)
+Espresso=Button(pestaña3,text="Espresso",bg="#255748",command=xp)
+Espresso.place(x=100,y=50)
+Frappe=Button(pestaña3,text="Frappe",bg="#255748",command=F)
+Frappe.place(x=170,y=50)
+Matcha=Button(pestaña3,text="Matcha",bg="#255748",command=mt)
+Matcha.place(x=230,y=50)
+Pizza=Button(pestaña3,text="Pizza",bg="#255748",command=c120)
+Pizza.place(x=50,y=100)
+Hamburguesa=Button(pestaña3,text="Hamburguesa",bg="#255748",command=H)
+Hamburguesa.place(x=100,y=100)
+HotDog=Button(pestaña3,text="HotDog",bg="#255748",command=HD)
+HotDog.place(x=190,y=100)
+Sandwich=Button(pestaña3,text="Sandwich",bg="#255748",command=chanwis)
+Sandwich.place(x=250,y=100)
+Cheesecake=Button(pestaña3,text="Cheesecake",bg="#255748",command=pay)
+Cheesecake.place(x=50,y=150)
+Pay=Button(pestaña3,text="Pay",bg="#255748",command=ck)
+Pay.place(x=130,y=150)
+
+
+if Metodo.get()=="Tarjeta":
+    MET= "Tarjeta"
+else:
+    MET = "Efectivo"
+pagar = Button(pestaña3,text="Pagar",bg="#255748",command=ejecutaInsert3)
+pagar.place(x=220,y=180)
+panel.add(pestaña1,text="Formulario usuarios")
+panel.add(pestaña2,text="Metodo de pago")
+panel.add(pestaña3,text="Menú")
 ventana.mainloop()
--- Proyecto Integrador/Registro_clase.py
+++ Proyecto Integrador/Registro_clase.py
@@ -1,15 +1,53 @@
 from tkinter import *
 from tkinter import messagebox
-class Registrar:
-    def __init__(self,Nombre,Apellidos,Correo,Telefono,Contraseña,vacio):
-        self.Nombre=Nombre
-        self.Apellidos=Apellidos
-        self.Correo=Correo
-        self.Telefono=Telefono
-        self.Contraseña=Contraseña
-        self.vacio=vacio
-    def registro(self):
-        if self.Nombre.get()==self.vacio or self.Apellidos.get()==self.vacio or self.Correo.get()==self.vacio or self.Telefono.get()==self.vacio or self.Contraseña.get()==self.vacio:
+import sqlite3
+class Registro_clase:
+    def __init__(self):
+        pass
+
+    def conexionDB (self):
+        try:
+            conexion = sqlite3.connect("C:/Users/Pablo/Documents/GitHub/PI/Proyecto Integrador/DBPI.db")
+            print("Conectado a la DB")
+            return conexion
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+    def registro(self,Nombre,Apellidos,Correo,Telefono,Contraseña,vacio):
+        conx = self.conexionDB()
+        if Nombre==vacio or Apellidos==vacio or Correo==vacio or Telefono==vacio or Contraseña==vacio:
             messagebox.showerror("Error","Se deben llenar todos los datos")
+            conx.close()
         else:
+            cursor = conx.cursor()
+            datos = (Nombre,Apellidos,Correo,Telefono,Contraseña)
+            sqlInsert = "insert into Usuarios(Nombre,Apellidos,Correo,Telefono,Contraseña) values (?,?,?,?,?)"
+            cursor.execute(sqlInsert,datos)
+            conx.commit()
+            conx.close
             messagebox.showinfo("Correcto","Sus datos se han registrado correctamente")
+    def Metod(self,Numero,Fecha,CVV,vacio):
+        conx = self.conexionDB()
+        if Numero==vacio or Fecha==vacio or CVV==vacio:
+            messagebox.showerror("Error","Se deben llenar todos los datos")
+            conx.close()
+        else:
+            cursor = conx.cursor()
+            datos = (Numero,Fecha,CVV)
+            sqlInsert = "insert into Tarjeta(Numero_tarjeta,Fecha_vencimiento,CVV) values (?,?,?)"
+            cursor.execute(sqlInsert,datos)
+            conx.commit()
+            conx.close
+            messagebox.showinfo("Correcto","Sus datos se han registrado correctamente")
+    def Pago(self,MET,Total):
+        conx = self.conexionDB()
+        if Total==0:
+            messagebox.showerror("Error","Se debe Elegir un producto")
+            conx.close()
+        else:
+            cursor = conx.cursor()
+            datos = (MET,Total)
+            sqlInsert = "insert into Compras(Metodo_pago,Total) values (?,?)"
+            cursor.execute(sqlInsert,datos)
+            conx.commit()
+            conx.close
+            messagebox.showinfo("Correcto","Se pagó un total de" + str(Total))
